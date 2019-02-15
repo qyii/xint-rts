6d48c5dc474f5595a3515b82af82c45cd09d061b
qyii <apupupupupu@gmail.com>
2019-02-15 18:19:11 +0800

    hello demo

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './Hello';
+import App from './pages/StatefulHello';
 
 ReactDOM.render(
   <App name='fubuki' />,
